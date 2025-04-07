f32c4b2817b8e23cec038ed18f349d2d594bebf1
Sci Alex <sim.AdhamZakaria23024@alexu.edu.eg>
2025-04-07 17:07:37 +0200

    Add project files.

---
+++ Demo.DataAccess/Data/Configurations/DepartmentComfiguration.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.DataAccess.Data.Configurations
+{
+    internal class DepartmentComfiguration : IEntityTypeConfiguration<Department>
+    {
+        public void Configure(EntityTypeBuilder<Department> builder)
+        {
+            builder.Property(dept=>dept.Id)
+                .UseIdentityColumn(10,10);
+
+            builder.Property(dept => dept.Name)
+                .HasColumnType("varchar(20)");
+
+            builder.Property(dept => dept.Code)
+                .HasColumnType("varchar(20)");
+
+            builder.Property(dept=> dept.CreatedOn).HasDefaultValueSql("GETDATE()");
+            // if the row is inserted without value , the default value will be used
+            // on insert
+            // can't reference other column
+            // can be overriden
+
+            builder.Property(dept => dept.LastModifiedOn).HasComputedColumnSql("GETDATE()");
+            // value is computed every time the record changed 
+            // on update
+            // can reference other column
+
+
+
+        }
+    }
+}
---
+++ Demo.DataAccess/Repositories/IDepartmentRepository.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Demo.DataAccess.Repositories
+{
+    public interface IDepartmentRepository
+    {
+        int Add(Department dept);
+        IEnumerable<Department> GetAll(bool IsTracking = false);
+        Department? GetById(int id);
+        int Remove(Department dept);
+        int Update(Department dept);
+    }
+}
